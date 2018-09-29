e225e065b17fb4a6173c9a86fa1020b6dc9cb1d0
assghost <527759038@qq.com>
2018-09-29 21:06:05 +0800

    feign 自定义 httpclient

--- feign-server/src/main/java/com/ghoject/cloud/feign/client/service/FeignConfiguration.java
+++ feign-server/src/main/java/com/ghoject/cloud/feign/client/service/FeignConfiguration.java
@@ -1,13 +1,12 @@
-package com.ghoject.cloud.feign.config;
+package com.ghoject.cloud.feign.client.service;
 
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
@@ -17,9 +16,10 @@ public class FeignConfiguration {
 
     /**
      * 自定义httpclient连接池，默认配置在FeignHttpClientProperties
+     * socketTimeout和connectTimeout以配置文件为准，在这里设置了不生效
      */
     @Bean
-    public HttpClient httpClient() {
+    public CloseableHttpClient httpClient() {
         System.out.println("===== 初始化 Apache httpclient 连接池 ====");
         // 生成默认请求配置
         RequestConfig.Builder requestConfigBuilder = RequestConfig.custom();
@@ -43,7 +43,7 @@ public class FeignConfiguration {
         httpClientBuilder.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy());
         httpClientBuilder.setConnectionManager(pollingConnectionManager);
         httpClientBuilder.setDefaultRequestConfig(defaultRequestConfig);
-        HttpClient client = httpClientBuilder.build();
+        CloseableHttpClient client = httpClientBuilder.build();
         // 启动定时器，定时回收过期的连接
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -59,5 +59,28 @@ public class FeignConfiguration {
         System.out.println("===== 初始化 Apache httpclient 连接池成功 ====");
         return client;
     }
+
+//    @Bean
+//    public HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory() {
+//        // httpClient连接配置，底层是配置RequestConfig
+//        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient());
+//        // 连接超时
+//        clientHttpRequestFactory.setConnectTimeout(5 * 1000);
+//        // 数据读取超时时间，即SocketTimeout
+//        clientHttpRequestFactory.setReadTimeout(30 * 1000);
+//        // 连接不够用的等待时间，不宜过长，必须设置，比如连接不够用时，时间过长将是灾难性的
+//        clientHttpRequestFactory.setConnectionRequestTimeout(200);
+//        // 缓冲请求数据，默认值是true。通过POST或者PUT大量发送数据时，建议将此属性更改为false，以免耗尽内存。
+//        // clientHttpRequestFactory.setBufferRequestBody(false);
+//        return clientHttpRequestFactory;
+//    }
+//
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate(){
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.setRequestFactory(httpComponentsClientHttpRequestFactory());
+//        return restTemplate;
+//    }
 
 }
--- feign-server/src/main/java/com/ghoject/cloud/feign/client/service/HelloFeignClient.java
+++ feign-server/src/main/java/com/ghoject/cloud/feign/client/service/HelloFeignClient.java
@@ -1,6 +1,5 @@
 package com.ghoject.cloud.feign.client.service;
 
-import com.ghoject.cloud.feign.config.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
